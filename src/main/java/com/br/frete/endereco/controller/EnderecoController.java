e7ce6f3e6bc7712f3954381706cd9fadba2552cb
Adriano Viera dos Santos <adrianovs.info@gmail.com>
2023-02-05 20:36:52 -0300

    Adicionado anotação para descrição da operação de consulta no Swagger

--- src/main/java/com/br/frete/endereco/controller/EnderecoController.java
+++ src/main/java/com/br/frete/endereco/controller/EnderecoController.java
@@ -2,6 +2,7 @@ package com.br.frete.endereco.controller;
 
 import com.br.frete.endereco.model.dto.EnderecoRequest;
 import com.br.frete.endereco.service.EnderecoService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ public class EnderecoController {
     private final EnderecoService enderecoService;
 
     @PostMapping("/consulta")
+    @Operation(summary = "Buscar o endereço e valor do frete pelo CEP", description = "Esta operação retorna os dados de endereço e valor do frete do referido CEP.")
     public ResponseEntity consultaCep(@RequestBody EnderecoRequest enderecoRequest) {
         return ResponseEntity.ok(enderecoService.executa(enderecoRequest));
     }
